6961290735e96c938ac0db17a68835f24c211f69
Richard Kombol <terungwakombol@gmail.com>
2017-01-07 08:56:24 +0100

    Eight tests pass

--- fizzbuzz/jasmine/src/library.js
+++ fizzbuzz/jasmine/src/library.js
@@ -1,4 +1,16 @@
 
-fizzBuzz: function (number) {
-   
+function fizzBuzz (number) {
+   if (number % 5 === 0 && number % 3 === 0){
+      return 'FizzBuzz';
+   }
+   if (number % 3 === 0 ){
+      return 'Fizz';
+   }
+   else if(number % 5 === 0) {
+      return 'Buzz';
+   }
+
+   else {
+      return number;
+   }
  }
