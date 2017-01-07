7fecd836d2423a23761cbc3845a13df7c3ea1539
Richard Kombol <terungwakombol@gmail.com>
2017-01-07 09:51:25 +0100

    Three more tests pass

--- arithmetic-and-geometric-progression/jasmine/src/library.js
+++ arithmetic-and-geometric-progression/jasmine/src/library.js
@@ -5,7 +5,7 @@ function aritGeo(arr) {
    }
    for (i=0; i < arr.length; i++) {
       if ( arr[i+1] - arr[i] === arr[i+2] - arr[i+1]){
-         return 'This is an array';
+         return 'Arithmetic';
       }
 
    }
