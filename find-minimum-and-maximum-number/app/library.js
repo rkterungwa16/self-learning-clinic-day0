f834b1a9c2cfafd87f75dc680d2290fef13b7ada
Richard Kombol <terungwakombol@gmail.com>
2017-01-07 02:27:35 +0100

    Test now passes for all four conditions

--- find-minimum-and-maximum-number/app/library.js
+++ find-minimum-and-maximum-number/app/library.js
@@ -4,6 +4,9 @@ module.exports = {
  
   findMinMax: function (arr) {
   	var minMaxArray = [];
+  	if (arr.length === 0) {
+  		return [];
+  	}
  	minMaxArray.push(arr[0]);
  	minMaxArray.push(arr[1]);
 	return minMaxArray;
