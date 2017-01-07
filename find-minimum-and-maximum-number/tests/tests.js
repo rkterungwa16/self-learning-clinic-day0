7c80a7faba3c9e3949262ee6c3c0bcba577d6b13
Richard Kombol <terungwakombol@gmail.com>
2017-01-07 02:35:28 +0100

    Test failure for finding the minimum and maximum number in a given array and returning an array of both values

--- find-minimum-and-maximum-number/tests/tests.js
+++ find-minimum-and-maximum-number/tests/tests.js
@@ -13,6 +13,7 @@ describe('Find Minimum and Maximum', function() {
 	it('Should return an array' , function() {
 		expect(myApp.findMinMax([])).to.be.an('array');
 	})
+  
   it('Should expect an empty array to return empty', function() {
     expect(myApp.findMinMax([])).to.be.empty;
   })
@@ -20,7 +21,12 @@ describe('Find Minimum and Maximum', function() {
   it('Element of array should be a number', function() {
     expect(myApp.findMinMax([1])[0]).to.be.a('Number');
   })
+
   it('Should return an array length of at most 2', function() {
     expect(myApp.findMinMax([1,3,4,5])).to.have.length.of.at.most(2);
   })
+
+  it('should return [1,4] for [1, 2, 3 , 4]', function () {
+    expect(findMinMax([1, 2, 3, 4])).toEqual([1, 4]);
+  });
 })
