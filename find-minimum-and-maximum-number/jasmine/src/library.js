e709e7fdef5c942f2ee0b02b3da04663db9bc673
Richard Kombol <terungwakombol@gmail.com>
2017-01-07 09:13:53 +0100

    All test pass for minimum and maximum function

--- find-minimum-and-maximum-number/jasmine/src/library.js
+++ find-minimum-and-maximum-number/jasmine/src/library.js
@@ -1,16 +1,26 @@
 
-function fizzBuzz (number) {
-   if (number % 5 === 0 && number % 3 === 0){
-      return 'FizzBuzz';
-   }
-   if (number % 3 === 0 ){
-      return 'Fizz';
-   }
-   else if(number % 5 === 0) {
-      return 'Buzz';
+function findMinMax (arr) {
+   var minMaxArray = [];
+   if (arr.length === 0) {
+      return [];
    }
 
-   else {
-      return number;
+   var maxNumber = Number.MIN_VALUE;
+   var minNumber = Number.MAX_VALUE;
+   for (var i=0; i<arr.length; i++) {
+      if (arr[i] > maxNumber) {
+         maxNumber = arr[i];
+      }
+      if (arr[i] < minNumber) {
+         minNumber = arr[i];
+      }
    }
+   if (maxNumber === minNumber) {
+      minMaxArray.push(minNumber);
+      return minMaxArray;
+   }
+
+   minMaxArray.push(minNumber);
+   minMaxArray.push(maxNumber);
+   return minMaxArray;
  }
--- fizzbuzz/jasmine/src/library.js
+++ fizzbuzz/jasmine/src/library.js
@@ -1,3 +1,16 @@
 
-function findMinMax(number) {
+function fizzBuzz (number) {
+   if (number % 5 === 0 && number % 3 === 0){
+      return 'FizzBuzz';
+   }
+   if (number % 3 === 0 ){
+      return 'Fizz';
+   }
+   else if(number % 5 === 0) {
+      return 'Buzz';
+   }
+
+   else {
+      return number;
+   }
  }
