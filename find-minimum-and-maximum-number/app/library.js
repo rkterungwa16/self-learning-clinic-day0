8b06180e332b53693547f388a89a5968007d7bff
Richard Kombol <terungwakombol@gmail.com>
2017-01-07 03:01:27 +0100

    Test case for uniform array element values now passes

--- find-minimum-and-maximum-number/app/library.js
+++ find-minimum-and-maximum-number/app/library.js
@@ -18,6 +18,10 @@ module.exports = {
 			minNumber = arr[i];
 		}
 	}
+	if (maxNumber === minNumber) {
+		minMaxArray.push(minNumber);
+		return minMaxArray;
+	}
 
  	minMaxArray.push(minNumber);
  	minMaxArray.push(maxNumber);
