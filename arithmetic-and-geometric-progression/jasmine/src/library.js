7b36cf6d6db85f869eedc29f4dfd3d55d30200e6
Richard Kombol <terungwakombol@gmail.com>
2017-01-07 10:20:22 +0100

    All tests pass

--- arithmetic-and-geometric-progression/jasmine/src/library.js
+++ arithmetic-and-geometric-progression/jasmine/src/library.js
@@ -4,7 +4,7 @@ function aritGeo(arr) {
       return 0;
    }
    for (i=0; i < arr.length; i++) {
-      if ( arr[i+1] - arr[i] === arr[i+2] - arr[i+1]){
+      if ( arr[i+1] - arr[i] === arr[i+2] - arr[i+1] && arr[i+3] - arr[i+2] === arr[i+2] - arr[i+1]){
          return 'Arithmetic';
       }
       else if ( arr[i+1]/arr[i] === arr[i+2]/arr[i+1]) {
