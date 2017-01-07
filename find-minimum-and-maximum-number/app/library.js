737c63da02ab2ddd4e614956ba83265c292495c0
Richard Kombol <terungwakombol@gmail.com>
2017-01-07 02:25:06 +0100

    Test passes for returning at most 2 condition, and fails for previous returning of an empty array

--- find-minimum-and-maximum-number/app/library.js
+++ find-minimum-and-maximum-number/app/library.js
@@ -3,7 +3,9 @@
 module.exports = {
  
   findMinMax: function (arr) {
- 	var minMaxArray = arr;
+  	var minMaxArray = [];
+ 	minMaxArray.push(arr[0]);
+ 	minMaxArray.push(arr[1]);
 	return minMaxArray;
   }
 }
--- find-minimum-and-maximum-number/tests/tests.js
+++ find-minimum-and-maximum-number/tests/tests.js
@@ -13,4 +13,14 @@ describe('Find Minimum and Maximum', function() {
 	it('Should return an array' , function() {
 		expect(myApp.findMinMax([])).to.be.an('array');
 	})
+  it('Should expect an empty array to return empty', function() {
+    expect(myApp.findMinMax([])).to.be.empty;
+  })
+
+  it('Element of array should be a number', function() {
+    expect(myApp.findMinMax([1])[0]).to.be.a('Number');
+  })
+  it('Should return an array length of at most 2', function() {
+    expect(myApp.findMinMax([1,3,4,5])).to.have.length.of.at.most(2);
+  })
 })
